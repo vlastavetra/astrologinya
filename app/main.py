43fbf79d2a080569d964baa72b5535253852ac6c
Vlasta Vetra <vlastavetra@gmail.com>
2024-01-21 22:43:24 +0200

    del not working libs

--- app/main.py
+++ app/main.py
@@ -3,20 +3,12 @@ import sys
 from pathlib import Path
 import uvicorn
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from motor.motor_asyncio import AsyncIOMotorClient
 from starlette.middleware import Middleware
 from starlette.responses import RedirectResponse
 
 sys.path.append(Path(__file__).parents[1].as_posix())
-
-load_dotenv()
-
-MONGO_URL = MONGO_URL = os.getenv("MONGO_URL")
-if MONGO_URL is None:
-    raise ValueError("MONGO_URL is not set. Please set it in .env file.")
 
 ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -32,7 +24,6 @@ middleware = [
 ]
 
 app = FastAPI(middleware=middleware)
-app.client = AsyncIOMotorClient(MONGO_URL)
 
 
 @app.on_event("startup")
